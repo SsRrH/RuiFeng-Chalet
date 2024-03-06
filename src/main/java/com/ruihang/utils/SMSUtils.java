3c7b5005dae2ca884931f496c2379c447ea376cc
SRH <15046492679@163.com>
2024-03-06 13:33:45 +0800

    Initial commit

--- src/main/java/com/ruihang/utils/SMSUtils.java
+++ src/main/java/com/ruihang/utils/SMSUtils.java
@@ -12,11 +12,11 @@ public class SMSUtils {
 
 	//发短信
 	public static void sendMessage(String signName, String templateCode,String phoneNumbers,String param){
-		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI5t8zUvdKkFD4PorNGs3s", "G7VMOZ2e768hGeqa4x4g3KeiidWZmo");
+		DefaultProfile profile = DefaultProfile.getProfile("111", "111", "111");
 		IAcsClient client = new DefaultAcsClient(profile);
 
 		SendSmsRequest request = new SendSmsRequest();
-		request.setSysRegionId("cn-hangzhou");
+		request.setSysRegionId("111");
 		request.setPhoneNumbers(phoneNumbers);
 		request.setSignName(signName);
 		request.setTemplateCode(templateCode);
