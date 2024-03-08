51eff89fa468733661e08f5b49d30ebe7e52096e
SsRrH <15046492679@163.com>
2024-03-08 17:41:49 +0800

    最新提交

--- src/main/java/com/ruihang/controller/UserController.java
+++ src/main/java/com/ruihang/controller/UserController.java
@@ -1,51 +1,51 @@
-//package com.ruihang.controller;
-//
-//import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-//import com.ruihang.common.R;
-//import com.ruihang.entity.User;
-//import com.ruihang.service.UserService;
-//import com.ruihang.utils.ValidateCodeUtils;
-//import lombok.extern.slf4j.Slf4j;
-//import org.apache.commons.lang.StringUtils;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import javax.servlet.http.HttpSession;
-//import java.util.Map;
-//
-//@RestController
-//@RequestMapping("/user")
-//@Slf4j
-//public class UserController {
-//
-//    @Autowired
-//    private UserService userService;
-//
-//
-//    //发送手机短信验证码
-//    @PostMapping("/sendMsg")
-//    public R<String> sendMsg(@RequestBody User user, HttpSession session){
-//        //获取手机号
-//        String phone = user.getPhone();
-//
-//        if(StringUtils.isNotEmpty(phone)){
-//            //生成随机的4位验证码
-//            String code = ValidateCodeUtils.generateValidateCode(4).toString();
-//            log.info("code={}",code);
-//
-//            //需要将生成的验证码保存到Session
-//            session.setAttribute(phone,code);
-//
-//            return R.success("手机验证码短信发送成功");
-//        }
-//
-//        return R.error("短信发送失败");
-//    }
-//
-//
+package com.ruihang.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.ruihang.common.R;
+import com.ruihang.entity.User;
+import com.ruihang.service.UserService;
+import com.ruihang.utils.ValidateCodeUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+@Slf4j
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+    //发送手机短信验证码
+    @PostMapping("/sendMsg")
+    public R<String> sendMsg(@RequestBody User user, HttpSession session){
+        //获取手机号
+        String phone = user.getPhone();
+
+        if(StringUtils.isNotEmpty(phone)){
+            //生成随机的4位验证码
+            String code = ValidateCodeUtils.generateValidateCode(4).toString();
+            log.info("code={}",code);
+
+            //需要将生成的验证码保存到Session
+            session.setAttribute(phone,code);
+
+            return R.success("手机验证码短信发送成功");
+        }
+
+        return R.error("短信发送失败");
+    }
+
+
 //    //移动端用户登录
 //    @PostMapping("/login")
 //    public R<User> login(@RequestBody Map map, HttpSession session){
@@ -76,5 +76,5 @@
 //        }
 //        return R.error("登录失败");
 //    }
-//
-//}
+
+}
